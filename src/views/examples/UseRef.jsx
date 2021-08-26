0bc6b11da917e4e4cbf02d53fde8e2ebf49742f0
jaisonrobson <xd_jrg@hotmail.com>
2021-08-26 11:49:01 -0300

    #Aula68 - Utilizado useRefs em Campos individualmente & feito desafio para concatenar valor dos campos alternando entre eles

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -2,15 +2,31 @@ import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
+const merge = (s1, s2) => [...s1].map((e, i) => `${e}${s2[i] || ""}`).join("")
+
 const UseRef = (props) => {
     const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
     const count = useRef(0)
+    const input1 = useRef(null)
+    const input2 = useRef(null)
 
     // count.current = count.current + 1
 
     useEffect(
-        () => count.current = count.current + 1,
+        () => {
+            count.current = count.current + 1
+            input2.current.focus()
+        },
         [value1]
+    )
+
+    useEffect(
+        () => {
+            count.current = count.current + 1
+            input1.current.focus()
+        },
+        [value2]
     )
 
     return (
@@ -26,7 +42,7 @@ const UseRef = (props) => {
                 <div>
                     <span className="text">Valor: </span>
 
-                    <span className="text">{value1} [</span>
+                    <span className="text">{merge(value1, value2)} [</span>
 
                     <span className="text red">{count.current}</span>
 
@@ -34,10 +50,23 @@ const UseRef = (props) => {
                 </div>
 
                 <input
+                    ref={input1}
                     type="text"
                     className="input"
                     value={value1}
                     onChange={(e) => setValue1(e.target.value)}
+                />
+            </div>
+
+            <SectionTitle title="Exercicio #02" />
+
+            <div className="center">
+                <input
+                    ref={input2}
+                    type="text"
+                    className="input"
+                    value={value2}
+                    onChange={e => setValue2(e.target.value)}
                 />
             </div>
         </div>
