2cd195ef60f408807353b340126c0378d982d5cf
jaisonrobson <xd_jrg@hotmail.com>
2021-09-01 18:08:23 -0300

    #Aula79 - criado hook personalizado para carregar dados json de url externa

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 import { useCounter } from '../../hooks/useCounter'
+import { useFetch } from '../../hooks/useFetch'
 
 const UseRef = (props) => {
     const [count, inc, dec] = useCounter()
+    const response = useFetch('http://files.cod3r.com.br/curso-react/estados.json')
+
+    function showStates(states) {
+        return states.map(state => <li key={state.nome}>{state.nome} - {state.sigla}</li>)
+    }
 
     return (
         <div className="UseCustom">
@@ -33,6 +39,14 @@ const UseRef = (props) => {
                         -1
                     </button>
                 </div>
+            </div>
+
+            <SectionTitle title="Exercicio #02" />
+
+            <div className="center">
+                <ul>
+                    {response.data ? showStates(response.data) : false}
+                </ul>
             </div>
         </div>
     )
